46e5b6e8c615d6bb6f6c81bd3c40b34ad5187ef8
Viktor Sivulskiy <svicvg@gmail.com>
2017-12-30 11:39:43 +0600

    NativeScript Angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { MenuComponent } from './menu/menu.component';
+
+import { DishService } from './services/dish.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+
+import { baseURL } from './shared/baseurl';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
-// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 @NgModule({
     bootstrap: [
@@ -23,11 +25,12 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
     ],
     providers: [
-        ItemService
+        {provide: 'BaseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
---
+++ app/shared/baseurl.ts
@@ -0,0 +1,1 @@
+export const baseURL = "http://192.168.1.10:3000/";
