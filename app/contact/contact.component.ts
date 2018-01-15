c66152e73aa800de735a337021b53ab7c9fd2f2c
Viktor Sivulskiy <svicvg@gmail.com>
2018-01-15 11:18:15 +0600

    Sending Email

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { DrawerPage } from '../shared/drawer/drawer.page';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
 
 @Component({
     selector: 'app-contact',
@@ -8,8 +10,26 @@ import { DrawerPage } from '../shared/drawer/drawer.page';
 })
 export class ContactComponent extends DrawerPage {
 
-    constructor(private changeDetectorRef: ChangeDetectorRef) {
+    constructor(private changeDetectorRef: ChangeDetectorRef,
+        private fonticon: TNSFontIconService) {
         super(changeDetectorRef);
     }
+
+    sendEmail() {
+
+        Email.available()
+          .then((avail: boolean) => {
+            if (avail) {
+              Email.compose({
+                to: ['confusion@food.net'],
+                subject: '[ConFusion]: Query',
+                body: 'Dear Sir/Madam:'
+              });
+            }
+            else
+              console.log('No Email Configured');
+          })
+    
+      }
 
 }
