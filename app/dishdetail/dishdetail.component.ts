ddf98e4f8825632025d97211bc2ed84c8ffd8db4
Viktor Sivulskiy <svicvg@gmail.com>
2018-01-15 11:30:48 +0600

    Social Sharing

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -15,9 +15,10 @@ import { View } from "ui/core/view";
 import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
 import { Color } from 'color';
 import * as enums from "ui/enums";
-
 import { CommentComponent } from "../comment/comment.component";
 import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
+import * as SocialShare from "nativescript-social-share";
+import { ImageSource, fromUrl } from "image-source";
 
 @Component({
   selector: 'app-dishdetail',
@@ -81,12 +82,24 @@ export class DishdetailComponent implements OnInit {
     this.routerExtensions.back();
   }
 
+  socialShare() {
+    let image: ImageSource;
+
+    fromUrl(this.BaseURL + this.dish.image)
+     .then((img: ImageSource) => {
+       image = img; 
+        SocialShare.shareImage(image, "How would you like to share this image?")
+      })
+     .catch(()=> { console.log('Error loading image'); });
+
+  }
+
   displayActionDialog() {
     let options = {
       title: "Actions",
       message: "Choose your action",
       cancelButtonText: "Cancel",
-      actions: ["Add to Favorites", "Add Comment"]
+      actions: ["Add to Favorites", "Add Comment", "Social Sharing"]
     };
 
     action(options).then((result) => {
@@ -95,6 +108,8 @@ export class DishdetailComponent implements OnInit {
         this.addToFavorites();
       } else if (result == "Add Comment") {
         this.createModalView();
+      } else if (result === 'Social Sharing') {
+        this.socialShare();
       }
     });
   }
